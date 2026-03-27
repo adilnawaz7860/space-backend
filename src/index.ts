b7edccff3ff5e0b4a7b59f0c5bcdeb1016e831d2
adil nawaz <adil.aishol3@gmail.com>
2026-03-27 16:08:51 +0530

    new push

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,8 @@ const app = new Hono();
 const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
 
 app.use("*", cors({
-  origin: FRONTEND_URL,
+  origin: "https://space-frontend-jlw4.vercel.app",
+
   allowMethods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowHeaders: ["Content-Type", "Authorization"],
   credentials: true,
